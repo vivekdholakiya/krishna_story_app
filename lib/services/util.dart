a24d550a5fce6ddc2d928c473e0cfb7dcd456061
vdholakiya105 <vdholakiya105@gmail.com>
2026-09-07 16:42:33 +0530

    ready to upload ios

--- lib/services/util.dart
+++ lib/services/util.dart
@@ -28,21 +28,29 @@ const String appStoreUrl =
 
 String get appShareUrl => Platform.isIOS ? appStoreUrl : playStoreUrl;
 
-// ── Ad Unit IDs ──────────────────────────────────────────────────
+// ── Android Ad Unit IDs ──────────────────────────────────────────────────
 
-// const String interstitialId = 'ca-app-pub-8791243074795894/7940378110';
-// const String rewardedAdId = 'ca-app-pub-8791243074795894/6627296447';
-// const String bannerAdId = 'ca-app-pub-8791243074795894/5576540829';
-// const String openAdId = 'ca-app-pub-8791243074795894/4123208297';
-// const String nativeAdId = 'ca-app-pub-8791243074795894/8301407241';
+const String interstitialId = 'ca-app-pub-8791243074795894/7940378110';
+const String rewardedAdId = 'ca-app-pub-8791243074795894/6627296447';
+const String bannerAdId = 'ca-app-pub-8791243074795894/5576540829';
+const String openAdId = 'ca-app-pub-8791243074795894/4123208297';
+const String nativeAdId = 'ca-app-pub-8791243074795894/8301407241';
+
+// ── IOS Ad Unit IDs ──────────────────────────────────────────────────
+
+const String IosInterstitialId = 'ca-app-pub-8791243074795894/1143498148';
+const String IosRewardedAdId = 'ca-app-pub-8791243074795894/5239972473';
+const String IosBannerAdId = 'ca-app-pub-8791243074795894/1196785686';
+const String IosOpenAdId = 'ca-app-pub-8791243074795894/7570622346';
+const String IosNativeAdId = 'ca-app-pub-8791243074795894/3284621923';
 
 //── Test Ad Unit IDs ─────────────────────────────────────────────
 
-const String interstitialId = 'ca-app-pub-3940256099942544/1033173712';
-const String rewardedAdId = 'ca-app-pub-3940256099942544/5224354917';
-const String bannerAdId = 'ca-app-pub-3940256099942544/6300978111';
-const String openAdId = 'ca-app-pub-3940256099942544/9257395921';
-const String nativeAdId = 'ca-app-pub-3940256099942544/2247696110';
+// const String interstitialId = 'ca-app-pub-3940256099942544/1033173712';
+// const String rewardedAdId = 'ca-app-pub-3940256099942544/5224354917';
+// const String bannerAdId = 'ca-app-pub-3940256099942544/6300978111';
+// const String openAdId = 'ca-app-pub-3940256099942544/9257395921';
+// const String nativeAdId = 'ca-app-pub-3940256099942544/2247696110';
 
 // ── Data Cache ───────────────────────────────────────────────────
 
